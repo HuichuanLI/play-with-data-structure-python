f58d1052a6e054e4df0d9796bc7ea4c6080cdec1
huichuan <lhc14124908@163.com>
2019-09-24 01:07:06 +0800

    add

--- chapter1 Sort/InsertionSort.py
+++ chapter1 Sort/InsertionSort.py
@@ -1,4 +1,5 @@
-from SortTestHelper import RandomArray
+from SortTestHelper import RandomArray, NearlyOrderArray
+from SelectionSort import selectionSort
 import time
 
 
@@ -12,17 +13,46 @@ def insertionSort(alist):
     return alist
 
 
+def insertionSortUpdate(alist):
+    for i in range(1, len(alist)):
+        currentvalue = alist[i]
+        for j in range(i, -1, -1):
+            if alist[j - 1] > currentvalue:
+                alist[j] = alist[j - 1]
+            else:
+                break
+        alist[j] = currentvalue
+    return alist
+
+
 if __name__ == "__main__":
     time_start = time.time()
-    
+
     nums = RandomArray()
     nums = insertionSort(nums)
     time_end = time.time()
     print('totally cost', time_end - time_start)
 
     a = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-    a = insertionSort(a)
+    a = insertionSortUpdate(a)
     for w in a:
         print(w)
+
     # for w in nums:
     #     print(w)
+
+    time_start = time.time()
+
+    nums = NearlyOrderArray(swap_times=100, number=10000)
+    nums = insertionSortUpdate(nums)
+    time_end = time.time()
+    print('Insertion totally cost', time_end - time_start)
+
+    time_start = time.time()
+
+    nums = NearlyOrderArray(swap_times=100, number=10000)
+    nums = selectionSort(nums)
+    time_end = time.time()
+    print('Selection totally cost', time_end - time_start)
+
+# 可见插入排序对近乎有序的数组很好
