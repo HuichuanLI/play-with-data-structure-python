eb19bf6e488c20bd4e42674fc3d840fdde7dd894
huichuan <lhc14124908@163.com>
2019-09-27 01:11:58 +0800

    add

--- README.md
+++ README.md
@@ -8,5 +8,7 @@ Please feel free to contact me if you have any questions with this repo:)
 | 1-1 选择排序 - Selection Sort |[python](https://github.com/HuichuanLI/play-with-data-structure-python/blob/master/chapter1%20Sort/SelectionSort.py)|
 | 1-2 插入排序 - Insertion Sort |[python](https://github.com/HuichuanLI/play-with-data-structure-python/blob/master/chapter1%20Sort/InsertionSort.py)|
 | 1-3 冒泡排序 - Bubble Sort |[python](https://github.com/HuichuanLI/play-with-data-structure-python/blob/master/chapter1%20Sort/BubbleSort.py)|
-| 1-4 插入排序 - Merge Sort |[python](https://github.com/HuichuanLI/play-with-data-structure-python/blob/master/chapter1%20Sort/MergeSort.py)|
-| 1-4 插入排序 - Merge Sort Optimastion |[python](https://github.com/HuichuanLI/play-with-data-structure-python/blob/master/chapter1%20Sort/MergeSortOptimastion.py)|
+| 1-4 归并排序 - Merge Sort |[python](https://github.com/HuichuanLI/play-with-data-structure-python/blob/master/chapter1%20Sort/MergeSort.py)|
+| 1-5 归并排序优化 - Merge Sort Optimastion |[python](https://github.com/HuichuanLI/play-with-data-structure-python/blob/master/chapter1%20Sort/MergeSortOptimastion.py)|
+| 1-6 快速排序 - Quick Sort |[python](https://github.com/HuichuanLI/play-with-data-structure-python/blob/master/chapter1%20Sort/QuickSort.py)|
+| 1-7 快速排序优化 - Quick Sort Optimastion |[python](https://github.com/HuichuanLI/play-with-data-structure-python/blob/master/chapter1%20Sort/QuickSortOptimsation.py)|
---
+++ chapter1 Sort/QuickSort.py
@@ -0,0 +1,69 @@
+from SortTestHelper import RandomArray, NearlyOrderArray
+from SelectionSort import selectionSort
+from InsertionSort import insertionSort, insertionSortRange
+import time
+
+import sys
+
+sys.setrecursionlimit(1000000)
+
+
+#
+def __quickSort(alist, left, right):
+    if right - left <= 15:
+        return insertionSortRange(alist, left, right)
+    p = None
+    p = __partion(alist, left, right)
+    __quickSort(alist, left, p - 1)
+    __quickSort(alist, p + 1, right)
+    return alist
+
+
+# 对 arr[l,r] 进行partiton 操做
+def __partion(alist, left, right):
+    current = alist[left]
+    j = left
+
+    for i in range(j + 1, right + 1):
+        if alist[i] < current:
+            alist[j + 1], alist[i] = alist[i], alist[j + 1]
+            j += 1
+    alist[j], alist[left] = alist[left], alist[j]
+
+    return j
+
+
+def quickSort(alist):
+    alist = __quickSort(alist, 0, len(alist) - 1)
+    return alist
+
+
+if __name__ == "__main__":
+    time_start = time.time()
+
+    nums = RandomArray()
+    nums = quickSort(nums)
+    time_end = time.time()
+    print('totally cost', time_end - time_start)
+
+    a = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+    a = quickSort(a)
+    for w in a:
+        print(w)
+
+    # for w in nums:
+    #     print(w)
+
+    time_start = time.time()
+
+    nums = NearlyOrderArray(swap_times=0, number=5000)
+    nums = quickSort(nums)
+    time_end = time.time()
+    print('Quick sort totally cost', time_end - time_start)
+
+    time_start = time.time()
+
+    nums = NearlyOrderArray(swap_times=0, number=5000)
+    nums = insertionSort(nums)
+    time_end = time.time()
+    print('Insertion sort totally cost', time_end - time_start)
