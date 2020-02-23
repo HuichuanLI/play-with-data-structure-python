c41b7b9dc1f073ded84ef7c9b54993de741a6228
huichuan <lhc14124908@163.com>
2020-02-23 17:06:33 +0800

    add

---
+++ Chapter9_SET_MAP/BSTSet.py
@@ -0,0 +1,42 @@
+from base import SetBase
+from BST import BST
+
+
+class BSTSet(SetBase):
+    def __init__(self):
+        self._bst = BST()
+
+    def get_size(self):
+        return self._bst.size()
+
+    def is_empty(self):
+        return self._bst.is_empty()
+
+    def add(self, e):
+        return self._bst.add(e)
+
+    def contains(self, e):
+        return self._bst.contains(e)
+
+    def remove(self, e):
+        return self._bst.remove(e)
+
+
+if __name__ == '__main__':
+    words = ''
+    with open('./shakespeare.txt', 'r') as f:
+        words = f.read()
+    words = words.split()
+
+    from time import time
+
+    start_time = time()
+    bst_set = BSTSet()
+    for word in words:
+        bst_set.add(word)
+
+    print('Total words: ', len(words))
+    print('Unique words: ', bst_set.get_size())
+    print('Contains word "they": ', bst_set.contains('they'))
+    ## 耗时0.58秒左右
+    print('Total time: {} seconds'.format(time() - start_time))
---
+++ Chapter9_SET_MAP/LinkedListSet.py
@@ -0,0 +1,44 @@
+from base import SetBase
+from LinkedList import LinkedList
+
+
+class LinkedListSet(SetBase):
+    def __init__(self):
+        self._list = LinkedList()
+
+    def get_size(self):
+        return self._list.get_size()
+
+    def is_empty(self):
+        return self._list.is_empty()
+
+    def contains(self, e):
+        return self._list.contains(e)
+
+    def add(self, e):
+        if self.contains(e):
+            return
+        self._list.add_first(e)
+
+    def remove(self, e):
+        self._list.remove(e)
+
+
+if __name__ == '__main__':
+    words = ''
+    with open('./shakespeare.txt', 'r') as f:
+        words = f.read()
+    words = words.split()
+
+    from time import time
+
+    start_time = time()
+    bst_set = LinkedListSet()
+    for word in words:
+        bst_set.add(word)
+
+    print('Total words: ', len(words))
+    print('Unique words: ', bst_set.get_size())
+    print('Contains word "they": ', bst_set.contains('they'))
+    ## 耗时100秒左右
+    print('Total time: {} seconds'.format(time() - start_time))
